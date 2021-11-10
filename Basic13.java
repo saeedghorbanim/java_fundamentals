41f1fd4329dcb2ecade3bf8e66d7952d153682ad
saeedghorbanim <animation10733@yahoo.com>
2021-11-10 17:37:23 -0600

    added finding small value in array

--- Basic13.java
+++ Basic13.java
@@ -1,8 +1,10 @@
 public class Basic13 {
     public static void main(String[] args){
         // basic1();
-        basic2(25);
+        // basic2(25);
 
+        int[] new_arr ={-1, 2, 5, -3, 7};
+        smallest(new_arr);
     }
 
     public static void basic1()
@@ -20,6 +22,15 @@ public class Basic13 {
             sum = sum + i;
         }
         System.out.println(sum);
+    }
 
+    public static void smallest(int arr[]){
+        int min = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
     }
 }
